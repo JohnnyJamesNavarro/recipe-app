de0a35a7443530da24c57a81a2a3168a675adec5
Johnny Navarro <johnnyjamesnavarro@gmail.com>
2020-02-21 17:26:27 -0500

    Created basic screens and added font loading.

---
+++ screens/FilterScreen.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function FilterScreen() {
+  return (
+    <View style={styles.screen}>
+      <Text>Filter Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
