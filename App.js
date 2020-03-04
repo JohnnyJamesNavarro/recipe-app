21fd22a584d047db414876957e36c1855a0c5f6c
Johnny Navarro <johnnyjamesnavarro@gmail.com>
2020-03-04 11:30:21 -0500

    Redux setup.

--- App.js
+++ App.js
@@ -3,11 +3,21 @@ import { StyleSheet, Text, View } from "react-native";
 import * as Font from "expo-font";
 import { AppLoading } from "expo";
 import { enableScreens } from "react-native-screens";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
 
 import MealsNavigator from "./navigation/MealsNavigator";
+import mealsReducer from "./store/reducers/meals";
 
 // Use optimized screen configurations for each native platform.
 enableScreens();
+
+// In case we had multiple reducers
+const rootReducer = combineReducers({
+  meals: mealsReducer
+});
+
+const store = createStore(rootReducer);
 
 const fetchFonts = () => {
   return Font.loadAsync({
@@ -28,5 +38,9 @@ export default function App() {
     );
   }
 
-  return <MealsNavigator />;
+  return (
+    <Provider store={store}>
+      <MealsNavigator />
+    </Provider>
+  );
 }
