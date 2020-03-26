637ffcb0976a03187c08f6686ab4adbd52561300
JohnnyJamesNavarro <johnnyjamesnavarro@gmail.com>
2020-03-25 20:53:13 -0500

    Finished Favorites functionality.

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { useSelector } from "react-redux";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import MealList from "../components/MealList";
 import CustomHeaderButton from "../components/CustomHeaderButton";
-
-import { useSelector } from "react-redux";
+import DefaultText from "../components/DefaultText";
 
 export default function FavoritesScreen(props) {
   // Getting the favorite meals from the redux store
   const favoriteMeals = useSelector(state => state.meals.favoriteMeals);
+
+  if (favoriteMeals.length === 0 || !favoriteMeals) {
+    return (
+      <View style={styles.emptyMessage}>
+        <DefaultText>No favorites yet, please add some.</DefaultText>
+      </View>
+    );
+  }
 
   return <MealList listData={favoriteMeals} navigation={props.navigation} />;
 }
@@ -29,3 +38,11 @@ FavoritesScreen.navigationOptions = navData => {
     )
   };
 };
+
+const styles = StyleSheet.create({
+  emptyMessage: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
